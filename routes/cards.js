03d31084e444ee59a52a8bd1cfb21f4edc603e77
maik791277 <potili@bk.ru>
2023-07-17 20:15:02 +0200

    Создание БД спринт 14 V0.7

--- routes/cards.js
+++ routes/cards.js
@@ -1,21 +1,38 @@
+const { celebrate, Joi, Segments } = require('celebrate');
 const express = require('express');
-
 const router = express.Router();
 const cardsController = require('../controllers/cards');
 
-// Получить все карточки
+// Валидация запроса на получение всех карточек
 router.get('/', cardsController.getCards);
 
-// Создать карточку
-router.post('/', cardsController.createCard);
+// Валидация запроса на создание карточки
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().regex(/^(https?:\/\/)?(www\.)?[-a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=]+#?$/),
+  }),
+}), cardsController.createCard);
 
-// Удаления карточки
-router.delete('/:cardId', cardsController.deleteCard);
+// Валидация запроса на удаление карточки
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), cardsController.deleteCard);
 
-// Поставить лайк карточке
-router.put('/:cardId/likes', cardsController.likeCard);
+// Валидация запроса на поставление лайка карточке
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), cardsController.likeCard);
 
-// Убрать лайк с карточки
-router.delete('/:cardId/likes', cardsController.dislikeCard);
+// Валидация запроса на удаление лайка с карточки
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), cardsController.dislikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,35 @@
+const { celebrate, Joi, Segments } = require('celebrate');
 const express = require('express');
 
 const router = express.Router();
 const usersController = require('../controllers/users');
 
-// Получить всех пользователей
+// Валидация запроса на получение всех пользователей
 router.get('/', usersController.getUsers);
 
+// Валидация запроса на получение информации о текущем пользователе
 router.get('/me', usersController.getUserInfo);
 
-// Получить пользователя по _id
-router.get('/:userId', usersController.getUserById);
+// Валидация запроса на получение пользователя по _id
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24),
+  }),
+}), usersController.getUserById);
 
-// Обновления профиля пользовтеля
-router.patch('/me', usersController.updateProfile);
+// Валидация запроса на обновление профиля пользователя
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+  }),
+}), usersController.updateProfile);
 
-// Обновить аватар пользователя
-router.patch('/me/avatar', usersController.updateAvatar);
+// Валидация запроса на обновление аватара пользователя
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().uri().regex(/^(https?:\/\/)?(www\.)?[-a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=]+#?$/).required(),
+  }),
+}), usersController.updateAvatar);
 
 module.exports = router;
